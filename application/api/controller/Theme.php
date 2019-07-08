c3a9382e3811713cf988b190770634f87d8e0daf
ismdeep <ismdeep@live.com>
2019-07-08 10:58:29 +0800

    feat: add theme selector-api /api/theme/select-theme?theme=mincss

---
+++ application/api/controller/Theme.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: ismdeep
+ * Date: 2019-07-08 10:56
+ */
+
+namespace app\api\controller;
+
+
+use app\extra\controller\ApiBaseController;
+
+class Theme extends ApiBaseController
+{
+    public function select_theme($theme = 'bootstrap')
+    {
+        session('theme_root', 'extra@themes/'.$theme);
+        return json([
+            'code' => 0,
+            'msg' => '操作成功',
+            'data' => $theme
+        ]);
+    }
+}
