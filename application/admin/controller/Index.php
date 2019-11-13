6d5e569f086de9d9808b028004e79c527a8b6ebc
ismdeep <ismdeep@live.com>
2019-11-13 08:30:39 +0800

    feat: 完善后台欢迎界面显示信息细节。

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,6 +7,8 @@ use app\api\model\ProblemModel;
 use app\api\model\SolutionModel;
 use app\api\model\UserModel;
 use app\extra\controller\AdminBaseController;
+use think\App;
+use function Sodium\version_string;
 
 class Index extends AdminBaseController
 {
@@ -34,8 +36,12 @@ class Index extends AdminBaseController
         $solution_count = (new SolutionModel())->count();
         // AC数量
         $ac_count = (new SolutionModel())->where(['result' => 4])->count();
-        // 硬盘剩余信息
+
+        // / 硬盘剩余信息
         $free_space = round(disk_free_space('/') / 1024 / 1024 / 1024, 2);
+
+        // /home 硬盘剩余信息
+        $free_space_home = round(disk_free_space('/home') / 1024 / 1024 / 1024, 2);
 
         $this->assign('problem_count', $problem_count);
         $this->assign('contest_count', $contest_count);
@@ -44,6 +50,8 @@ class Index extends AdminBaseController
         $this->assign('user_count', $user_count);
         $this->assign('ac_count', $ac_count);
         $this->assign('free_space', $free_space);
+        $this->assign('free_space_home', $free_space_home);
+        $this->assign('thinkphp_version', THINK_VERSION);
 
         return view();
     }
