6710501caf128967dba185256310971c5178e570
ismdeep <ismdeep@live.com>
2020-02-29 07:56:59 +0800

    feat: 登录日志功能

--- application/api/controller/Login.php
+++ application/api/controller/Login.php
@@ -9,10 +9,12 @@
 namespace app\api\controller;
 
 
+use app\api\model\LoginLogModel;
 use app\api\model\PrivilegeModel;
 use app\api\model\UserModel;
 use app\extra\controller\ApiBaseController;
 use app\extra\util\PasswordUtil;
+use think\Request;
 use think\Session;
 
 class Login extends ApiBaseController
@@ -33,23 +35,25 @@ class Login extends ApiBaseController
      */
 	public function login($username,$password)
 	{
+	    $request = Request::instance();
 		$user = UserModel::get(['user_id' => $username]);
 		if (!$user) return json(['status' => 'error', 'msg' => $this->lang['user_not_exists']]);
 		if (PasswordUtil::check_password($password, $user->password)) {
 			Session::set('user', $user);
-
 			// 判断是否是管理员administrator
 			if (PrivilegeModel::get(['user_id' => $username, 'rightstr' => 'administrator'])) {
 				Session::set('administrator', $user);
 			}
-
 			// 判断是否是root账号
 			if (PrivilegeModel::get(['user_id' => $username, 'rightstr' => 'root'])) {
 				Session::set('root', $user);
 			}
-
+			// 添加登录日志
+			LoginLogModel::push($user->user_id, $request->ip(), $request->header('user-agent'), 1);
 			return json(['status' => 'success', 'msg' => $this->lang['login_success']]);
 		}else{
+            // 添加登录日志
+            LoginLogModel::push($user->user_id, $request->ip(), $request->header('user-agent'), 1);
 			return json(['status' => 'error', 'msg' => $this->lang['wrong_password']]);
 		}
 	}
