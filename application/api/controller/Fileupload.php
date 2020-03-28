0af022ebac0973e0bb2436cbf2378f91c6b1cbce
ismdeep <ismdeep@live.com>
2020-03-29 07:52:58 +0800

    v2020.03.29 文件上传恢复使用本地存储

--- application/api/controller/Fileupload.php
+++ application/api/controller/Fileupload.php
@@ -21,33 +21,11 @@ class Fileupload extends ApiBaseController {
         $file = request()->file('file');
         $info = $file->move('upload/' . $this->loginuser->user_id . '/' . date("Ymd") . '/', false, true);
 
-        vendor('aliyun-oss-php-sdk.autoload');
-        $oss_client = new OssClient('XhYqlheIeMqs4vYM', 'krF5kKnOjra94e9g00fKHgtU4Zvzzt', 'http://oss-cn-shenzhen.aliyuncs.com');
-        $bucket = 'ismdeep';
 
-        $obj = 'justoj-data/' . $this->loginuser->user_id . '/' . date('Ymd') . '/' . $info->getSaveName();
-        $file_path = $info->getPath() . '/' . $info->getSaveName();
-        try {
-            $oss_client->uploadFile($bucket, $obj, $file_path);
-
-            $upload = new UploadModel();
-            $upload->user_id = $this->loginuser->user_id;
-            $upload->oss_path = $obj;
-            $upload->file_size = $info->getSize();
-            $upload->save();
-
-            unlink($file_path);
-
-            return json([
+        return json([
                 'code' => 0
-                , 'data' => "https://ismdeep.oss-cn-shenzhen.aliyuncs.com/{$obj}"
+                , 'data' => "/{$info->getPath()}/{$info->getSaveName()}"
             ]);
-        } catch (OssException $e) {
-            return json([
-                'code' => 502
-                , 'msg' => '无法上传，系统错误。'
-            ]);
-        }
     }
 
 }
