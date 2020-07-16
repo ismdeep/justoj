0df2ac058a030f59522eac4bc59d6aa3a784e0fe
ismdeep <ismdeep@protonmail.com>
2020-07-16 10:00:57 +0800

    Refactor: ContestProblemModel.php

--- application/api/model/ContestProblemModel.php
+++ application/api/model/ContestProblemModel.php
@@ -13,14 +13,43 @@ use think\Db;
 use think\Exception;
 use think\Model;
 
-class ContestProblemModel extends Model
-{
-	protected $table = 'contest_problem';
-	public function fk()
-	{
-		$this->problem = ProblemModel::get(['problem_id' => $this->problem_id]);
-		// 获得这场比赛中AC题目的人数
-		$this->problem->ac_cnt = Db::query("select count(solution_id) as cnt from solution where contest_id=".$this->contest_id." and problem_id=".$this->problem->problem_id." and result=4")[0]["cnt"];
-		$this->problem->submit_cnt = Db::query("select count(solution_id) as cnt from solution where contest_id=".$this->contest_id." and problem_id=".$this->problem->problem_id)[0]["cnt"];
-	}
+
+/**
+ * Class ContestProblemModel
+ * @package app\api\model
+ *
+ * @property int problem_id
+ * @property int contest_id
+ * @property string title
+ * @property int num
+ * @property \DateTime create_time
+ * @property \DateTime update_time
+ *
+ */
+class ContestProblemModel extends Model {
+    protected $table = 'contest_problem';
+
+    /**
+     * @return ProblemModel
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function get_problem() {
+        /* @var $problem ProblemModel */
+        $problem = (new ProblemModel())->where('problem_id', $this->problem_id)->find();
+        return $problem;
+    }
+
+    /**
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function fk() {
+        $this->problem = $this->get_problem();
+        // 获得这场比赛中AC题目的人数
+        $this->problem->ac_cnt = Db::query("select count(solution_id) as cnt from solution where contest_id=" . $this->contest_id . " and problem_id=" . $this->problem->problem_id . " and result=4")[0]["cnt"];
+        $this->problem->submit_cnt = Db::query("select count(solution_id) as cnt from solution where contest_id=" . $this->contest_id . " and problem_id=" . $this->problem->problem_id)[0]["cnt"];
+    }
 }
--- application/api/model/GroupJoinModel.php
+++ application/api/model/GroupJoinModel.php
@@ -11,12 +11,26 @@ namespace app\api\model;
 
 use think\Model;
 
-class GroupJoinModel extends Model
-{
-	protected $table = 'group_join';
+/**
+ * Class GroupJoinModel
+ * @package app\api\model
+ *
+ * @property int id
+ * @property string user_id
+ * @property int group_id
+ * @property int status
+ * @property int deleted
+ * @property \DateTime create_time
+ * @property \DateTime update_time
+ */
+class GroupJoinModel extends Model {
+    protected $table = 'group_join';
 
-	public function fk()
-	{
-		$this->group = GroupModel::get(['id' => $this->group_id]);
-	}
+    public function get_group() {
+        return (new GroupModel())->where(['id' => $this->group_id])->find();
+    }
+
+    public function fk() {
+        $this->group = $this->get_group();
+    }
 }
