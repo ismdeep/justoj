c33cd14dfdcd6f7aa88cce824ae15c4a5095cf37
ismdeep <ismdeep@protonmail.com>
2020-09-25 16:35:38 +0800

    refactor: 重构班级作业页面进度条

--- application/api/model/GroupTaskModel.php
+++ application/api/model/GroupTaskModel.php
@@ -26,4 +26,43 @@ use think\Model;
  */
 class GroupTaskModel extends Model {
     protected $table = 'group_task';
+
+    /**
+     * 获取班级平均完成题目数
+     */
+    public function getTotalDoneCnt() {
+        /* @todo 未完成 */
+        return (new SolutionModel())
+            ->where('contest_id', $this->contest_id)
+            ->where('result', SolutionModel::RESULT_AC)
+            ->group('problem_id,user_id')
+            ->count();
+    }
+
+    /**
+     * 获取学生完成题目数
+     *
+     * @param string $user_id 用户ID
+     * @return int|string
+     * @throws \think\Exception
+     */
+    public function getDoneCntByUserId($user_id = '') {
+        /* @todo 需要完善只筛选在作业合法时间段提交的完成数量 */
+        return (new SolutionModel())
+            ->where('contest_id', $this->contest_id)
+            ->where('user_id', $user_id)
+            ->where('result', SolutionModel::RESULT_AC)
+            ->group('problem_id')
+            ->count('problem_id');
+    }
+
+    /**
+     * 获取题目数量
+     */
+    public function getProblemCnt() {
+        return (new ContestProblemModel())
+            ->where('contest_id', $this->contest_id)
+            ->count();
+    }
+
 }
