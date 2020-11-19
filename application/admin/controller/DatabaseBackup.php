af7b5c326bc02acb93bddd20f6b4ad7e90ec3952
ismdeep <l.jiang.1024@gmail.com>
2020-11-19 20:31:52 +0800

    feat: Add DatabaseBackup.php

---
+++ application/admin/controller/DatabaseBackup.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\extra\controller\AdminBaseController;
+use think\Env;
+
+class DatabaseBackup extends AdminBaseController {
+    public function do_backup_json() {
+        // 判断系统是否有 mysqldump 进程在运行
+        $results = [];
+        exec("ps aux | grep mysqldump | grep -v grep | wc -l", $results);
+        if ($results[0] != "0") {
+            return json([
+                'code' => 500,
+                'msg' => '系统正在执行数据库备份，请稍后再试。'
+            ]);
+        }
+        $file_name = sprintf("justoj-%d.sql.gz", time());
+        $cmd = sprintf("bash %s %s %s >/dev/null 2>/dev/null &",
+            Env::get('database.backup_sh'),
+            Env::get('database.backup_dir'),
+            $file_name
+        );
+        exec($cmd);
+
+        return json([
+            'code' => 0,
+            'msg' => 'success',
+            'file_name' => $file_name
+        ]);
+    }
+
+    public function is_mysqldump_running_json() {
+        $results = [];
+        $cmd = sprintf("ps aux | grep mysqldump | grep -v grep | wc -l");
+        exec($cmd, $results);
+        $is_running = $results[0] == "0" ? false : true;
+
+        return json([
+            'code' => 0,
+            'data' => $is_running
+        ]);
+    }
+
+    public function remove_backup_file_json($filename = '') {
+        if (!$filename) {
+            return json([
+                'code' => 500,
+                'msg' => 'filename can not be empty.'
+            ]);
+        }
+
+        unlink(Env::get('database.backup_dir') . '/' . $filename);
+        return json(['code' => 0, 'msg' => 'success']);
+    }
+}
