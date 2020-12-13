f4ee9ce8a18f9118eae203451f714406c4176918
ismdeep <l.jiang.1024@gmail.com>
2020-12-13 09:05:16 +0800

    修复界面语言切换的问题 (#45)
    Closes #45

--- application/api/controller/Uilanguage.php
+++ application/api/controller/Uilanguage.php
@@ -11,13 +11,14 @@ namespace app\api\controller;
 
 use app\api\model\UiLanuageModel;
 use app\api\common\ApiBaseController;
+use app\api\model\UserModel;
 use think\Session;
 
 class Uilanguage extends ApiBaseController {
     public function set_lang($language = '') {
         if ('' == $language) return json(['status' => 'error', 'msg' => 'Arguments error.']);
         if ('cn' == $language || 'en' == $language) {
-            // 判断用户是否登录
+            /* 判断用户是否登录 */
             if ($this->login_user) {
                 $uilanguage = UiLanuageModel::get(['user_id' => $this->login_user->user_id]);
                 if ($uilanguage) {
@@ -29,6 +30,10 @@ class Uilanguage extends ApiBaseController {
                     $uilanguage->language = $language;
                     $uilanguage->save();
                 }
+                /* @var $user UserModel */
+                $user = (new UserModel())->where('user_id', $this->login_user->user_id)->find();
+                $user->fk_session_info();
+                Session::set('user', $user);
             } else {
                 Session::set('ui_language', $language);
             }
