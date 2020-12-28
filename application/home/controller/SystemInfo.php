6ba7737c6104980f779fa03e18929284f39bc6ac
L. Jiang <l.jiang.1024@gmail.com>
2020-12-28 10:32:51 +0800

    feat: 将 git hash 字符串处理成 7 位短 hash 字符串

--- application/home/controller/SystemInfo.php
+++ application/home/controller/SystemInfo.php
@@ -11,23 +11,39 @@ use think\Env;
 
 class SystemInfo extends HomeBaseController {
     public function index() {
-
-
-
-
         return view($this->theme_root . '/system-info');
     }
 
     public function project_hash_part() {
+        $branch_name = exec("git symbolic-ref --short -q HEAD");
+        $this->assign('branch_name', $branch_name);
+
+        $release_date = exec("git log -1 --pretty=format:%at");
+        $release_date = date('Y-m-d H:i:s', $release_date);
+        $this->assign('release_date', $release_date);
+
         $source_code_hash = exec("git log -1 --pretty=format:%H");
+        $source_code_hash = substr($source_code_hash, 0, 7);
         $this->assign('source_code_hash', $source_code_hash);
+
         return view($this->theme_root . '/system-info-project-hash-part');
     }
 
     public function data_hash_part() {
-        // 获取服务器上数据git hash
+
         $data_dir = Env::get('config.data_dir');
+
+        $branch_name = exec("cd {$data_dir};git symbolic-ref --short -q HEAD");
+        $this->assign('branch_name', $branch_name);
+
+        $release_date = exec("cd {$data_dir};git log -1 --pretty=format:%at");
+        $release_date = date('Y-m-d H:i:s', $release_date);
+        $this->assign('release_date', $release_date);
+
+        // 获取服务器上数据git hash
+
         $local_hash = exec("cd {$data_dir};git log -1 --pretty=format:%H");
+        $local_hash = substr($local_hash, 0, 7);
         $this->assign('local_hash', $local_hash);
 
         // 判断服务器是否有数据未同步
@@ -35,6 +51,10 @@ class SystemInfo extends HomeBaseController {
         $local_changed = $local_changed ? true : false;
         $this->assign('local_changed', $local_changed);
         $judge_clients = (new JudgeClientModel())->order('client_name', 'asc')->select();
+        foreach ($judge_clients as $client) {
+            /* @var $client JudgeClientModel */
+            $client->data_git_hash = substr($client->data_git_hash, 0, 7);
+        }
         $this->assign('judge_clients', $judge_clients);
 
         return view($this->theme_root . '/system-info-data-hash-part');
