4e40ed5cd820d7cc46ecc59a89e7f156079880fb
L. Jiang <l.jiang.1024@gmail.com>
2021-06-11 06:57:55 +0800

    feat: Update judgeclient updatetime
    Update judgeclient updatetime
    
    Log: Update judgeclient updatetime

--- application/api/controller/JudgeClient.php
+++ application/api/controller/JudgeClient.php
@@ -48,7 +48,7 @@ class JudgeClient extends ApiBaseController {
         }
 
         $judge_client->data_git_hash = $data_git_hash;
-        $judge_client->update_time = DateTime::DATENOW();
+        $judge_client->isUpdate(true);
         $judge_client->save();
 
         return json(['code' => 0, 'msg' => 'ok']);
